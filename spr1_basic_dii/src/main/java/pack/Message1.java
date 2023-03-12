f34e58afe9fa935a8c069470b89719192c7b31a0
Sukyung Lee <leesuukg@gmail.com>
2023-03-13 00:36:41 +0900

    first commit

---
+++ spr1_basic_dii/src/main/java/pack/Message1.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class Message1 implements MessageInter{
+	
+	public void sayHello(String name) {
+		System.out.println("안녕하세요 "+name+"씨");
+	}
+}
---
+++ spr1_basic_dii/src/main/java/pack/Message2.java
@@ -0,0 +1,8 @@
+package pack;
+
+public class Message2 implements MessageInter{
+	
+	public void sayHello(String name) {
+		System.out.println("반가워요 "+name+"님");
+	}
+}
